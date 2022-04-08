af4aa8c708266d6d756264cd075f71142fd05bd8
Yusuke Wada <yusuke@kamawada.com>
2022-04-08 11:20:31 +0900

    build

--- dist/index.js
+++ dist/index.js
@@ -13453,84 +13453,124 @@ module.exports = createFollowersPage;
 /***/ 2833:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
-const formatMarkdown = __nccwpck_require__(3164);
-const headerComponent = __nccwpck_require__(5091);
-const starComponent = __nccwpck_require__(8810);
-const socialMediaComponent = __nccwpck_require__(8272);
-const thirdPartyComponent = __nccwpck_require__(6433);
-const licenseComponent = __nccwpck_require__(9805);
+const formatMarkdown = __nccwpck_require__(3164)
+const headerComponent = __nccwpck_require__(5091)
+const starComponent = __nccwpck_require__(8810)
+const socialMediaComponent = __nccwpck_require__(8272)
+const thirdPartyComponent = __nccwpck_require__(6433)
+const licenseComponent = __nccwpck_require__(9805)
 let createIndexPage = (function () {
-    let createListOfCities = function (locationDataModel) {
-        let cities = ``;
-        for(const location of locationDataModel.locations) {
-            if(locationDataModel.country !== location) {
-                cities = cities + `\t\t\t<code>${formatMarkdown.capitalizeTheFirstLetterOfEachWord(location)}</code> \n`;
-            }
-        }
-        return cities;
+  let createListOfCities = function (locationDataModel) {
+    let cities = ``
+    for (const location of locationDataModel.locations) {
+      if (locationDataModel.country !== location) {
+        cities =
+          cities +
+          `\t\t\t<code>${formatMarkdown.capitalizeTheFirstLetterOfEachWord(location)}</code> \n`
+      }
     }
-    let createListOfCountriesAndCitiesTable = function (indexUrl, readConfigResponseModel) {
-        readConfigResponseModel.locations.sort((a,b) => a.country > b.country ? 1 : -1);
-        let table = `<table>\n`;
-        table = table + `\t<tr>\n`;
-        table = table + `\t\t<th>\n`;
-        table = table + `\t\t\tCountry/State\n`;
-        table = table + `\t\t</th>\n`;
-        table = table + `\t\t<th>\n`;
-        table = table + `\t\t\tCities\n`;
-        table = table + `\t\t</th>\n`;
-        table = table + `\t</tr>\n`;
-        for(const locationDataModel of readConfigResponseModel.locations) {
-            table = table + `\t<tr>\n`;
-            table = table + `\t\t<td>\n`;
-            table = table + `\t\t\t<a href="${indexUrl}/blob/main/markdown/public_contributions/${formatMarkdown.getCountryName(locationDataModel.country)}.md">\n`;
-            table = table + `\t\t\t\t${formatMarkdown.capitalizeTheFirstLetterOfEachWord(locationDataModel.country)}\n`;
-            table = table + `\t\t\t</a>\n`;
-            table = table + `\t\t</td>\n`;
-            table = table + `\t\t<td>\n`;
-            table = table + createListOfCities(locationDataModel);
-            table = table + `\t\t</td>\n`;
-            table = table + `\t</tr>\n`;
-        }
-        table = table + `</table>\n\n`;
-        return table;
+    return cities
+  }
+  let createListOfCountriesAndCitiesTable = function (indexUrl, readConfigResponseModel) {
+    readConfigResponseModel.locations.sort((a, b) => (a.country > b.country ? 1 : -1))
+    let table = `<table>\n`
+    table = table + `\t<tr>\n`
+    table = table + `\t\t<th>\n`
+    table = table + `\t\t\tCountry/State\n`
+    table = table + `\t\t</th>\n`
+    table = table + `\t\t<th>\n`
+    table = table + `\t\t\tCities\n`
+    table = table + `\t\t</th>\n`
+    table = table + `\t</tr>\n`
+    for (const locationDataModel of readConfigResponseModel.locations) {
+      table = table + `\t<tr>\n`
+      table = table + `\t\t<td>\n`
+      table =
+        table +
+        `\t\t\t<a href="${indexUrl}/blob/main/markdown/public_contributions/${formatMarkdown.getCountryName(
+          locationDataModel.country
+        )}.md">\n`
+      table =
+        table +
+        `\t\t\t\t${formatMarkdown.capitalizeTheFirstLetterOfEachWord(locationDataModel.country)}\n`
+      table = table + `\t\t\t</a>\n`
+      table = table + `\t\t</td>\n`
+      table = table + `\t\t<td>\n`
+      table = table + createListOfCities(locationDataModel)
+      table = table + `\t\t</td>\n`
+      table = table + `\t</tr>\n`
     }
-    let create = function (githubUsernameAndRepository, readConfigResponseModel) {
-        let markdown = headerComponent.create();
-        markdown = markdown + `<a href="https://gayanvoice.github.io/top-github-users/index.html">\n`;
-        markdown = markdown + `\t<img align="right" width="400" src="https://github.com/gayanvoice/top-github-users-monitor/raw/master/public/images/banner/top-github-users-map.png" alt="top-github-users-by-country">\n`;
-        markdown = markdown + `</a>\n\n`;
-        markdown = markdown + `List of most active GitHub users based on \`public contributions\` \`private contributions\` and \`number of followers\`  by country or state. `;
-        markdown = markdown + `The list updated \`${formatMarkdown.getDate()}\`.\n\n`;
-        markdown = markdown + `This repository contains users \`${readConfigResponseModel.locations.length} countries\` and \`${formatMarkdown.getNumberOfCities(readConfigResponseModel)} cities\`. \n`;
-        markdown = markdown + `To get into the list you need to have minimum number of followers that varies in each country. `;
-        markdown = markdown + `The list can be found in [config.json](https://github.com/${githubUsernameAndRepository}/blob/main/config.json).\n\n`;
-        markdown = markdown + `Contribute to GitHub action [gayanvoice/top-github-users-action](https://github.com/gayanvoice/top-github-users-action). `;
-        markdown = markdown + `The project maintained by [gayanvoice](https://github.com/gayanvoice). `
-        markdown = markdown + `Don't forget to follow him on [GitHub](https://github.com/gayanvoice), [Twitter](https://twitter.com/gayanvoice), and [Medium](https://gayanvoice.medium.com/).\n\n`;
-        markdown = markdown + starComponent.create();
-        markdown = markdown + `### 🚀 Share on\n\n`;
-        markdown = markdown + socialMediaComponent.create(
-            "Top GitHub Users By Country",
-            "List of most active github users based on public contributions, total contributions, and number of followers by country",
-            `https://github.com/${githubUsernameAndRepository}`);
-        markdown = markdown + createListOfCountriesAndCitiesTable(
-            `https://github.com/${githubUsernameAndRepository}`,
-            readConfigResponseModel);
-        markdown = markdown + `### 🚀 Share on\n\n`;
-        markdown = markdown + socialMediaComponent.create(
-            "Top GitHub Users By Country",
-            "List of most active github users based on public contributions, total contributions, and number of followers by country",
-            `https://github.com/${githubUsernameAndRepository}`);
-        markdown = markdown + thirdPartyComponent.create();
-        markdown = markdown + licenseComponent.create(githubUsernameAndRepository);
-        return markdown;
-    }
-    return {
-        create: create,
-    };
-})();
-module.exports = createIndexPage;
+    table = table + `</table>\n\n`
+    return table
+  }
+  let create = function (githubUsernameAndRepository, readConfigResponseModel) {
+    let markdown = headerComponent.create()
+
+    markdown = markdown + `### ただし日本に限る\n\n`
+    markdown =
+      markdown +
+      `<a href="https://github.com/gayanvoice/top-github-users">Top GitHub Users By Country</a>がおもしろいのですが、いかんせん日本（に限らないけど）の更新頻度が低くて、情報が古いので「日本に限った」ものを作りました。本家で使っている<a href="https://github.com/yusukebe/top-github-users-action">GitHub ActionsをForkしたもの</a>を使ってページを生成しています。もし気に入ったら<a href="https://github.com/gayanvoice/top-github-users">本家のレポジトリ</a>に ⭐ するのをお忘れなく。\n\n`
+
+    markdown = markdown + `<a href="https://gayanvoice.github.io/top-github-users/index.html">\n`
+    markdown =
+      markdown +
+      `\t<img align="right" width="400" src="https://github.com/gayanvoice/top-github-users-monitor/raw/master/public/images/banner/top-github-users-map.png" alt="top-github-users-by-country">\n`
+    markdown = markdown + `</a>\n\n`
+    markdown =
+      markdown +
+      `List of most active GitHub users based on \`public contributions\` \`private contributions\` and \`number of followers\`  by country or state. `
+    markdown = markdown + `The list updated \`${formatMarkdown.getDate()}\`.\n\n`
+    markdown =
+      markdown +
+      `This repository contains users \`${
+        readConfigResponseModel.locations.length
+      } countries\` and \`${formatMarkdown.getNumberOfCities(readConfigResponseModel)} cities\`. \n`
+    markdown =
+      markdown +
+      `To get into the list you need to have minimum number of followers that varies in each country. `
+    markdown =
+      markdown +
+      `The list can be found in [config.json](https://github.com/${githubUsernameAndRepository}/blob/main/config.json).\n\n`
+    markdown =
+      markdown +
+      `Contribute to GitHub action [gayanvoice/top-github-users-action](https://github.com/gayanvoice/top-github-users-action). `
+    markdown = markdown + `The project maintained by [gayanvoice](https://github.com/gayanvoice). `
+    markdown =
+      markdown +
+      `Don't forget to follow him on [GitHub](https://github.com/gayanvoice), [Twitter](https://twitter.com/gayanvoice), and [Medium](https://gayanvoice.medium.com/).\n\n`
+    markdown = markdown + starComponent.create()
+    markdown = markdown + `### 🚀 Share on\n\n`
+    markdown =
+      markdown +
+      socialMediaComponent.create(
+        'Top GitHub Users By Country',
+        'List of most active github users based on public contributions, total contributions, and number of followers by country',
+        `https://github.com/${githubUsernameAndRepository}`
+      )
+    markdown =
+      markdown +
+      createListOfCountriesAndCitiesTable(
+        `https://github.com/${githubUsernameAndRepository}`,
+        readConfigResponseModel
+      )
+    markdown = markdown + `### 🚀 Share on\n\n`
+    markdown =
+      markdown +
+      socialMediaComponent.create(
+        'Top GitHub Users By Country',
+        'List of most active github users based on public contributions, total contributions, and number of followers by country',
+        `https://github.com/${githubUsernameAndRepository}`
+      )
+    markdown = markdown + thirdPartyComponent.create()
+    markdown = markdown + licenseComponent.create(githubUsernameAndRepository)
+    return markdown
+  }
+  return {
+    create: create,
+  }
+})()
+module.exports = createIndexPage
+
 
 /***/ }),
 
