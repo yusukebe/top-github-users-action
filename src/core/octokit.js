55b893fc3c938a1c17652e4ad8ee3e886547c861
Yusuke Wada <yusuke@kamawada.com>
2022-04-08 10:49:24 +0900

    Update number of users

--- src/core/octokit.js
+++ src/core/octokit.js
@@ -1,15 +1,16 @@
-const { graphql } = require("@octokit/graphql");
-const OctokitResponseModel = require('../model/octokit/OctokitResponseModel');
-let octokit = function () {
-    let getHeader = function (AUTH_KEY) {
-       return  {
-            headers: {
-                authorization: `token ${AUTH_KEY}`,
-            },
-        }
+const { graphql } = require('@octokit/graphql')
+const OctokitResponseModel = require('../model/octokit/OctokitResponseModel')
+let octokit = (function () {
+  let getHeader = function (AUTH_KEY) {
+    return {
+      headers: {
+        authorization: `token ${AUTH_KEY}`,
+      },
     }
-    let getQuery = function (locations, numberOfUsers, cursor) {
-        return { query: `query {
+  }
+  let getQuery = function (locations, numberOfUsers, cursor) {
+    return {
+      query: `query {
               search(type: USER, query:"${locations} sort:followers-desc", first:${numberOfUsers}, after:${cursor}) {
                 edges {
                   node {
@@ -38,28 +39,28 @@ let octokit = function () {
                     hasNextPage
                   }
               }
-            }`};
+            }`,
     }
-    let setCursor = function (cursor) {
-        if(cursor === null){
-            return cursor
-        } else {
-            return `"${cursor}"`;
-        }
+  }
+  let setCursor = function (cursor) {
+    if (cursor === null) {
+      return cursor
+    } else {
+      return `"${cursor}"`
     }
-    let request = async function (AUTH_KEY, locations, cursor) {
-        try{
-            const graphqlWithAuth = graphql.defaults(getHeader(AUTH_KEY));
-            const response = await graphqlWithAuth(getQuery(locations, 10, setCursor(cursor)));
-            return new OctokitResponseModel(true, response);
-        } catch (error) {
-            console.log(error)
-            return new OctokitResponseModel(false)
-        }
-
+  }
+  let request = async function (AUTH_KEY, locations, cursor) {
+    try {
+      const graphqlWithAuth = graphql.defaults(getHeader(AUTH_KEY))
+      const response = await graphqlWithAuth(getQuery(locations, 5, setCursor(cursor)))
+      return new OctokitResponseModel(true, response)
+    } catch (error) {
+      console.log(error)
+      return new OctokitResponseModel(false)
     }
-    return {
-        request: request
-    };
-}();
-module.exports = octokit;
+  }
+  return {
+    request: request,
+  }
+})()
+module.exports = octokit
